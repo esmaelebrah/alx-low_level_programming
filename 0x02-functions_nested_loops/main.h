929a325d7325cf7980cbeb1dc46b7366620fd68e
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-02-21 06:32:51 -0800

    fhgd

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+void print_times_table(int n);
+<<<<<<< HEAD
+=======
+int _putchar(char c);
+>>>>>>> c4c8f6b1264a862a3edbc4cbcf0dad347efdf69c
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
