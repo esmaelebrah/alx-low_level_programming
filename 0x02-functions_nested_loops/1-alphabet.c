d7bb26b651fb313b7d015f2345d8afd0a3519f96
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-02-21 03:33:56 -0800

    nvb

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.c>
+/**
+ * ptint the alphabet
+ *  in lowercase, followed by a new line.
+ */
+int main()
+{
+	char ch;
+	ch = 'a'
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
