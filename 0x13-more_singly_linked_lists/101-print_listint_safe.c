a1f11c122c8438c3959be9e689e5eb0ed5c7b1f0
esmaelebrah <esmaelebrahimhasengmail.com>
2023-04-05 16:40:51 +0300

    farther updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,16 +8,18 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
+	size_t count = 0;
 	long int diff;
 
 	while (head)
 	{
 		diff = head - head->next;
-		num++;
+		count++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (diff > 0)
+		{
 			head = head->next;
+		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
@@ -25,5 +27,5 @@ size_t print_listint_safe(const listint_t *head)
 		}
 	}
 
-	return (num);
+	return (count);
 }
