57aab8b4761222348267c77e90b1cb445e4fc7fb
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-18 05:09:38 -0700

    updated

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t s = 0;
+size_t s = 0;
 
-	while(h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		s += 1;
-	}
+while(h)
+{
+printf("%d\n", h->n);
+h = h->next;
+s += 1;
+}
 
-	return s;
+return (s);
 }
